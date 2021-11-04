f6d6e6f1bfe20160b7dbff3ea4fbd6fe3e32d955
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-04 10:18:15 +0200

    pages created and imported

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # React router intro
 
-1. npm i bootstrap@^4.6
+1. npm i bootstrap@^4.6 2.https://v5.reactrouter.com/web/guides/quick-start
+2.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './styles/App.css';
 import Navbar from './components/Navbar';
+import AboutPage from './components/pages/About';
+import ContactPage from './components/pages/Contact';
+
+// create simple about component
+// create simple contact us component
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <h1>Lets router react</h1>
+      <div className='container'>
+        <h1>Lets router react</h1>
+        <AboutPage />
+        <ContactPage />
+      </div>
     </div>
   );
 }
